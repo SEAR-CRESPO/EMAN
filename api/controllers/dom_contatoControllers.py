e4d30f5398dfe78759a189ff7c29ce8db287fd7c
NIlson cristo <nilsoncristo0000@gmailo.com>
2026-08-28 15:23:47 -0500

    controllers

--- api/controllers/dom_contatoControllers.py
+++ api/controllers/dom_contatoControllers.py
@@ -4,3 +4,13 @@ from models.dom_contato import dom_contacto
 class dom_contatoControllers:
     data = dom_contacto.read()
     return jsonify({"mensaje":data}), 200
+
+ def add():
+     
+    data = request.get_jsonify(Silent=True)
+    
+    
+if not data:
+    return jsonify({"error": "No se proporcionaron datos"}), 400
+
+required = [""]
