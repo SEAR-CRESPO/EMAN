e83186cdb42c528214174e7b451520fef7a98615
ronyspava2 <pavaronys2@gmail.com>
2026-08-28 16:18:26 -0500

    iofkofjfjijfjfjfjuf fjf fjuf fhjfj

--- api/models/produc_carri.py
+++ api/models/produc_carri.py
@@ -16,4 +16,7 @@ class produc_carri:
             'PROC_PRO_ID'   : self.PROC_PRO_ID,
             'PROC_COMC_ID'  : self.PROC_COMC_ID
         }
+    def tiueie(self):
+        return{
         
+        }
