a2f78574491c3b01e14839c99b51f814ea939acd
Tu Nombre Completo <tu.correo@ejemplo.com>
2026-09-10 16:37:04 -0500

    actualizado

--- api/controllers/dom_contatoControllers.py
+++ api/controllers/dom_contatoControllers.py
@@ -2,18 +2,12 @@ from flask import jsonify, request
 from models.dom_contato import dom_contacto
 
 class dom_contatoControllers:
-<<<<<<< HEAD
     data = dom_contacto.read()
     return jsonify({"mensaje":data}), 200
 
-def add():
-     
-    data = request.get_jsonify(Silent=True)
-=======
     def read():
         data = dom_contacto.read()
         return jsonify({"mensaje": data}), 200
->>>>>>> Nilson
     
     def add():
         data = request.get_json(silent=True)
@@ -30,3 +24,6 @@ def add():
         
         x = dom_contacto.add(data)
         return x
+
+    def delete():
+        
